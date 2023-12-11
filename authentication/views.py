cdefd0b4068057eb687d5125d6ad92101808ebd0
Afza7 <114289348+Afza7@users.noreply.github.com>
2023-12-11 22:26:54 +0530

    Rearranging the files into the right directory

--- authentication/views.py
+++ authentication/views.py
@@ -4,11 +4,5 @@ from . import views
 def login(request):
     return render(request, 'login.html', {})
 
-def schedule(request):
-    return render(request, 'schedule.html', {})
-
 def signup(request):
     return render(request, 'signup.html', {})
-
-def resources(request):
-    return render(request, 'resources', {})
--- resources/urls.py
+++ resources/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name="login"),
-    path('signup/', views.signup, name="signup"),
+    path('resources/', views.resources, name="resources"),
 ]
