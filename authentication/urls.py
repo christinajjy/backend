3d4bd30fdb1117167d8dccd00d96cb2e6adb04dc
mounikagarrepelli <garrepellimounika13@gmail.com>
2023-12-13 11:41:04 +0530

    bootstrap navbar added to all html files

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+
 urlpatterns = [
     path('login/', views.login_user, name="login"),
     path('home/',views.home,name="home"),
     path('login/signup/', views.signup, name="signup"),
+    # path('base/',views.base,name="base")
     
 ]
