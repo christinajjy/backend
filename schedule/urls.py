eab5fa249a3802e38f5f7b740b0ce63669a614d0
smanasa <manasareddy2003@gmail.com>
2023-12-14 11:54:42 +0530

    added schedule functionality

--- schedule/urls.py
+++ schedule/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
+# scheduler_app/urls.py
+
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('schedule/', views.schedule, name="schedule"),
+    path("login/schedule/", views.schedule, name="schedule"),
 ]
