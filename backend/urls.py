58885b4904cc70fc9279529755a879933d2895e2
christina joseph jyothula <christinajosephjyothula@gmail.com>
2023-12-16 00:53:54 +0530

    UI changes

--- backend/urls.py
+++ backend/urls.py
@@ -21,8 +21,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('',include('django.contrib.auth.urls')),
     path('', include('authentication.urls')),
-    path('', include('resources.urls')),
-    path('', include('schedule.urls')),
-    path('', include('authentication.urls')),
+    path('', include('resources.urls',namespace="r")),
+    path('', include('schedule.urls',namespace="s")),
     path('', include('IT_Person.urls')),
 ]
