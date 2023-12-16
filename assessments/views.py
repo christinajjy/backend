01cdcf9a2a772ae06abf6b1839a8d25392c36026
Afza7 <114289348+Afza7@users.noreply.github.com>
2023-12-17 03:18:23 +0530

    Adding pie chart to the results

--- assessments/views.py
+++ assessments/views.py
@@ -4,7 +4,12 @@ from django.shortcuts import redirect,render
 from django.contrib.auth import login,logout,authenticate
 from .forms import *
 from .models import *
-from django.http import HttpResponse
+from django.shortcuts import render
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
+import matplotlib
+matplotlib.use('agg')
 
 
 def assessments(request):
@@ -33,14 +38,30 @@ def result(request):
                 correct+=1
             else:
                 wrong+=1
-        percent = score/(total*10) *100
+        percent = 100 - (score/(total*10) *100)
+        labels = ['Correct', 'Wrong']
+        sizes = [correct, wrong]
+        explode = (0.1, 0)  # explode 1st slice
+
+        fig1, ax1 = plt.subplots()
+        ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle
+
+        # Save the chart to a BytesIO buffer
+        chart_buffer = BytesIO()
+        plt.savefig(chart_buffer, format='png')
+        chart_buffer.seek(0)
+        chart_data = base64.b64encode(chart_buffer.getvalue()).decode('utf-8')
+        plt.close()
+
         context = {
             'score':score,
             'time': request.POST.get('timer'),
             'correct':correct,
             'wrong':wrong,
             'percent':percent,
-            'total':total
+            'total':total,
+            'chart_data': chart_data,
         }
         return render(request,'assessments/results.html',context)
     else:
@@ -49,3 +70,4 @@ def result(request):
             'question':question
         }
         return render(request,'assessments/assessments.html',context)
+
